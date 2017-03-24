55a8c723cb5d57b01c93dca89ed28167453cfbf1
leejh3224 <leejh3224@naver.com>
2017-03-25 02:37:23 +0900

    Update PageList.js

--- src/components/PageList.js
+++ src/components/PageList.js
@@ -2,19 +2,19 @@ import React from 'react';
 import PageItem from './PageItem';
 
 const PageList = (props) => {
-    const pageItem = props.pages.map((obj) => {
-      return <PageItem key={obj.pageid} pages={obj} />;
-    });
 
-    const ulStyle = {
-      listStyleType : "none"
-    };
-    
-  return(
-    <ul style={ulStyle}>
-      {pageItem}
-    </ul>
-  );
+    const pageItem = props.pages.map((obj) => {
+        return <PageItem
+                  key={obj.pageid}
+                  pages={obj}
+               />;
+        });
+
+    return(
+      <ul style={{listStyleType : "none"}}>
+        {pageItem}
+      </ul>
+    );
 }
 
 export default PageList;
